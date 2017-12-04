a9f9437d2d26edf9bf481b8fa5d4898b963b8cce
nbnsireesha <nbnsireesha@gmail.com>
2017-12-03 22:37:05 -0600

    half done

--- letter.js
+++ letter.js
@@ -5,11 +5,13 @@ var isLetter = require('is-letter');
 var Word = require("./word.js");
 //no of chances the player have
 var limit = 13; 
+var newWord = new Word();
 function Letter(){
 
+	var self = this;
+
 	this.getLetter = function(){
-		var newWord = new Word();
-		if(newWord.remainingLetters > 0 ){
+		if(newWord.remainingLetters > 0 && limit >= 1 ){
 			inquirer.prompt([
 			  {
 
@@ -25,25 +27,48 @@ function Letter(){
 			  	}
 			  }
 			]).then(function(answer){
-				console.log(answer.letter);
-				console.log(newWord.remainingLetters);
+				//console.log(answer.letter);
+				//console.log(newWord.remainingLetters);
 				//console.log(limit);
 				if(newWord.remainingLetters > 0 && limit >= 1){
 					for (var i = 0; i < newWord.randomWord.length; i++) {
 						debugger
-						if(newWord.randomWord[i] == answer.letter){
+						if(newWord.randomWord[i] == answer.letter && newWord.wordArray[i] == "_ "){
 
 							newWord.wordArray[i] = answer.letter;
-							console.log(newWord.wordArray.join());
+							console.log(newWord.wordArray.join().replace( /,/g, "" ));
+							console.log("CORRECT!!!");
+							newWord.remainingLetters--;
 							
 						}
+						// else if(newWord.randomWord[i] != answer.letter){
+						// 	console.log("INCORRECT!!!");
+						// }
+						else if(newWord.randomWord[i] == answer.letter && newWord.wordArray[i] != "_ "){
+							console.log("you choose one you have already choosen");
+							limit++;
+						}
 					}//end of for
+					limit--;
+					if(limit == 1){
+						console.log("its your last chance of guessing");
+					}
+					if(limit == 0){
+						console.log("you are out of lifes try again");
+						//reset game
+					}
+					var strwithCom = newWord.wordArray.join();
+					var guessedWord = strwithCom.replace( /,/g, "" );
+					if(newWord.randomWord == guessedWord){
+						console.log("congrates you won");
+					}
+					self.getLetter();
 				}//if
-				Letter();
+				
 			})
 		}
 		else{
-
+			return;
 		}
 	}
 	
